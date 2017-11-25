4bf9e8cbd47d05c72d959837ed6c8ad7ad1b285c
Salatfreak <d.r.accounts@gmx.de>
2017-11-26 00:35:40 +0100

    Configure tld length and cookie storage for i18n
    Set tld length for development and testing to 0 because 'localhost'
    doesn't have a tld.
    
    Setup the session store to share the session cookie among subdomains.
    
    Configure english as default locale and allow german as well.

---
+++ config/initializers/locale.rb
@@ -0,0 +1,5 @@
+# Set available locales
+I18n.available_locales = [:en, :de]
+
+# Make english the default locale
+I18n.default_locale = :en
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,1 @@
+Rails.application.config.session_store :cookie_store, key: '_animal_rights_network', domain: :all, tld_length: 1
