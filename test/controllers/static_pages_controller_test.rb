f23180f0a2581a46d2fbb7eb13224246b2e4bcce
Salatfreak <d.r.accounts@gmx.de>
2017-11-26 13:59:46 +0100

    Change request host in static pages test suite

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    host! 'animalrights.test'
+  end
+
   test "should get home" do
     get root_url
     assert_response :success
