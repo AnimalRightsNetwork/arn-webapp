b4d6e3ec70fee0d3cb7a3e682aadcb373c846ad0
Salatfreak <d.r.accounts@gmx.de>
2017-11-26 13:44:27 +0100

    Add tests for name and icon_url validations

---
+++ test/models/org_type_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class OrgTypeTest < ActiveSupport::TestCase
+  test "should only save with name and icon url" do
+    # Test without attributes
+    t = OrgType.new
+    assert_not t.save
+    assert_not_empty t.errors[:name]
+    assert_not_empty t.errors[:icon_url]
+
+    # Test without name
+    t.name = nil
+    t.icon_url = 'org_types/association.png'
+    assert_not t.save
+    assert_not_empty t.errors[:name]
+
+    # Test without icon url
+    t.name = 'association'
+    t.icon_url = nil
+    assert_not t.save
+    assert_not_empty t.errors[:icon_url]
+
+    # Test with attributes
+    t.name = 'association'
+    t.icon_url = 'org_types/association.png'
+    assert t.save
+  end
+
+  test "should enforce name uniqueness" do
+    t1 = OrgType.new name: 'party', icon_url: 'org_types/party.png'
+    t2 = OrgType.new name: 'party', icon_url: 'org_types/parties.png'
+    assert t1.save
+    assert_not t2.save
+    assert_not_empty t2.errors[:name]
+  end
+end
