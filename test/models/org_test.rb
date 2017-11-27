87d458307def32e490c7573b2ed514365370c685
Salatfreak <d.r.accounts@gmx.de>
2017-11-27 13:03:01 +0100

    Refactor organization model tests
    Adapt to new access strategy that only allows setting the id through the
    display_id.

--- test/models/org_test.rb
+++ test/models/org_test.rb
@@ -8,48 +8,75 @@ class OrgTest < ActiveSupport::TestCase
     assert_not_empty o.errors[:id]
 
     ## Test too short id
-    o = Org.new(default_options.merge id: 'sht', display_id: 'Sht', name: "ID Test 2")
+    o = Org.new(default_options.merge display_id: 'Sht', name: "ID Test 2")
     assert_not o.save
     assert_not_empty o.errors[:id]
 
     # Test too long id
-    o = Org.new(default_options.merge id: 'a' * 31, display_id: 'A' * 31, name: "ID Test 3")
+    o = Org.new(default_options.merge display_id: 'A' * 31, name: "ID Test 3")
     assert_not o.save
     assert_not_empty o.errors[:id]
     
     # Test lower limit
-    o = Org.new(default_options.merge id: 'abcd', display_id: 'Abcd', name: "ID Test 4")
+    o = Org.new(default_options.merge display_id: 'Abcd', name: "ID Test 4")
     assert o.save
 
     # Test upper limit
-    o = Org.new(default_options.merge id: 'a' * 30, display_id: 'A' * 30, name: "ID Test 5")
+    o = Org.new(default_options.merge display_id: 'A' * 30, name: "ID Test 5")
     assert o.save
 
     # Test invalid character
-    o = Org.new(default_options.merge id: 'HelloYou', display_id: 'HelloYou', name: "ID Test 6")
+    o = Org.new(default_options.merge display_id: 'Hello You', name: "ID Test 6")
     assert_not o.save
     assert_not_empty o.errors[:id]
   end
 
   test "should enforce id uniqueness" do
-    o1 = Org.new(default_options.merge id: 'duplicate', display_id: 'Duplicate', name: "ID Test 7")
-    o2 = Org.new(default_options.merge id: 'duplicate', display_id: 'DupliCate', name: "ID Test 8")
+    o1 = Org.new(default_options.merge display_id: 'Duplicate', name: "ID Test 7")
+    o2 = Org.new(default_options.merge display_id: 'DupliCate', name: "ID Test 8")
     assert o1.save
     assert_not o2.save
     assert_not_empty o2.errors[:id]
   end
 
-  test "should not save if id and display_id don't match" do
-    o = Org.new(default_options.merge id: 'displayidtest', display_id: 'DisplayId')
-    assert_not o.save
-    assert_not_empty o.errors[:display_id]
+  test "should only make id accessible through display_id" do
+    # Test id mass assignment
+    assert_raises NoMethodError do
+      o = Org.new(default_options.merge id: 'displayid', display_id: "DisplayID", name: "ID Test 9")
+    end
+
+    # Test id auto assignment
+    o = Org.new(default_options.merge display_id: 'DisplayId', name: "ID Test 9")
+    assert_equal o.id, o.display_id.downcase
+
+    # Test direct id assignment
+    assert_raises NoMethodError do
+      o.id = 'newid'
+    end
   end
 
+  test "should allow subsequent display_id assignment" do
+    assert_nothing_raised do
+      # Test without display id
+      o = Org.new(default_options.merge name: "ID Test 10")
+      assert_not o.save
+      assert_not_empty o.errors[:id]
+
+      # Test assigning nil
+      o.display_id = nil
+      assert_not o.save
+      assert_not_empty o.errors[:id]
+
+      # Test assigning valid id
+      o.display_id = 'ValidID'
+      assert o.save
+    end
+  end
 
   test "should not save without type" do
     # Test without type
     o = Org.new(
-      default_options.merge id: 'typeless', display_id: 'Typeless', name: "Typeless", type: nil
+      default_options.merge display_id: 'Typeless', name: "Typeless", type: nil
     )
     assert_not o.save
     assert_not_empty o.errors[:type]
@@ -58,21 +85,21 @@ class OrgTest < ActiveSupport::TestCase
   test "should not save without image" do
     # Test without logo url
     o = Org.new default_options.merge(
-      id: 'imageless', display_id: 'Imageless', name: "Imageless", logo_url: nil
+      display_id: 'Imageless', name: "Imageless", logo_url: nil
     )
     assert_not o.save
     assert_not_empty o.errors[:logo_url]
 
     # Test without cover url
     o = Org.new default_options.merge(
-      id: 'imageless', display_id: 'Imageless', name: "Imageless", cover_url: nil
+      display_id: 'Imageless', name: "Imageless", cover_url: nil
     )
     assert_not o.save
     assert_not_empty o.errors[:cover_url]
     
     # Test without marker url
     o = Org.new default_options.merge(
-      id: 'imageless', display_id: 'Imageless', name: "Imageless", marker_url: nil
+      display_id: 'Imageless', name: "Imageless", marker_url: nil
     )
     assert_not o.save
     assert_not_empty o.errors[:marker_url]
@@ -81,21 +108,21 @@ class OrgTest < ActiveSupport::TestCase
   test "should not save without valid color" do
     # Test without marker color
     o = Org.new default_options.merge(
-      id: 'colorless', display_id: 'Colorless', name: "Colorless", marker_color: nil
+      display_id: 'Colorless', name: "Colorless", marker_color: nil
     )
     assert_not o.save
     assert_not_empty o.errors[:marker_color]
 
     # Test without too short marker color
     o = Org.new default_options.merge(
-      id: 'colorless', display_id: 'Colorless', name: "Colorless", marker_color: "000"
+      display_id: 'Colorless', name: "Colorless", marker_color: "000"
     )
     assert_not o.save
     assert_not_empty o.errors[:marker_color]
 
     # Test without invalid characters
     o = Org.new default_options.merge(
-      id: 'colorless', display_id: 'Colorless', name: "Colorless", marker_color: "yellow"
+      display_id: 'Colorless', name: "Colorless", marker_color: "yellow"
     )
     assert_not o.save
     assert_not_empty o.errors[:marker_color]
@@ -103,7 +130,7 @@ class OrgTest < ActiveSupport::TestCase
 
   test "should not destroy type if associated with organization" do
     t = Org::Type.new name: 'association', icon_url: 'org_types/association.png'
-    o = Org.new(default_options.merge id: 'typed', display_id: 'Typed', name: "Typed", type: t)
+    o = Org.new(default_options.merge display_id: 'Typed', name: "Typed", type: t)
     assert t.save
     assert o.save
     assert_not t.destroy
