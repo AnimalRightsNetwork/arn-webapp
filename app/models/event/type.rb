9edf728386ba967ac52ff868f02d2da68258db04
Salatfreak <d.r.accounts@gmx.de>
2017-11-28 19:21:15 +0100

    Update event type foreign key in models

--- app/models/event/type.rb
+++ app/models/event/type.rb
@@ -1,6 +1,6 @@
 class Event::Type < ApplicationRecord
   # Associations
-  has_many :events, dependent: :restrict_with_error
+  has_many :events, foreign_key: :event_type_id, dependent: :restrict_with_error
 
   # Validate name uniqueness and presence
   validates :name, uniqueness: true, presence: true
