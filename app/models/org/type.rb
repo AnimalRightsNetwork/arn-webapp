ec39eae878ae6713c4c6c7172361e1765341e6db
Salatfreak <d.r.accounts@gmx.de>
2017-11-28 16:08:05 +0100

    Add comments to organization type model

--- app/models/org/type.rb
+++ app/models/org/type.rb
@@ -1,6 +1,10 @@
 class Org::Type < ApplicationRecord
+  # Association
   has_many :orgs, dependent: :restrict_with_error
 
+  # Validate name uniqueness and presence
   validates :name, uniqueness: true, presence: true
+
+  # Validate icon url presence
   validates :icon_url, presence: true
 end
