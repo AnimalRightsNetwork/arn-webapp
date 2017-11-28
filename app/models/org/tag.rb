3e42efd70f734e28832830a3844a10c5bb6c1575
Salatfreak <d.r.accounts@gmx.de>
2017-11-28 22:41:05 +0100

    Implement organization tag model

---
+++ app/models/org/tag.rb
@@ -0,0 +1,14 @@
+class Org::Tag < ApplicationRecord
+  # Associations
+  #has_and_belongs_to_many :orgs, foreign_key: :org_tag_id
+
+  # Validate uniqueness and presence of name
+  validates :name, uniqueness: true, presence: true
+
+  # Validate presence of icon url
+  validates :icon_url, presence: true
+
+  # Validate length, format and presence of color
+  validates :color, length: { is: 6 },
+    format: { with: /\A[0-9a-f]*\z/, error: :invalid_characters }, presence: true
+end
