8aa834bb93af48e0711c8ae703ee435a3b11e404
Salatfreak <d.r.accounts@gmx.de>
2017-11-28 22:04:14 +0100

    Add forgotten org association implementation

--- app/models/org.rb
+++ app/models/org.rb
@@ -2,6 +2,7 @@ class Org < ApplicationRecord
   # Associations
   belongs_to :type
   has_many :events, validate: false
+  has_many :descriptions, autosave: true, dependent: :destroy
   has_and_belongs_to_many :admins, class_name: :User, join_table: :org_administrations
 
   # Validate organization id length and character restrictions
@@ -11,6 +12,9 @@ class Org < ApplicationRecord
   # Validate name
   validates :name, length: { minimum: 4, maximum: 30 }, uniqueness: true, presence: true
   
+  # Validate description
+  validate :descriptions_not_empty
+
   # Validate url presence
   validates_presence_of :logo_url, :cover_url, :marker_url
 
@@ -24,4 +28,10 @@ class Org < ApplicationRecord
     self.id = display_id&.downcase
     super(display_id)
   end
+
+  # Helper methods
+  private
+    def descriptions_not_empty
+      errors.add(:descriptions, :empty) if descriptions.empty?
+    end
 end
