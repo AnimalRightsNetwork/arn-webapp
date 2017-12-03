5a6135b895f430f0a3fef654370f909f34e1fc27
Salatfreak <d.r.accounts@gmx.de>
2017-12-03 16:50:12 +0100

    Add controller and helper tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -6,7 +6,57 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get new" do
+    # Test without login
     get login_url
     assert_response :success
+    assert_select 'form'
+  end
+
+  test "should login once" do
+    # Login Alice
+    post login_url, params: {
+      session: {
+        email: 'Alice@example.com',
+        password: "AlicePassw0rd"
+      }
+    }
+    assert_redirected_to root_url
+    assert_includes flash.keys, 'success'
+
+    # Test login page when already logged in
+    get login_url
+    assert_redirected_to root_url
+    assert_includes flash.keys, 'warning'
+
+    # Login again
+    post login_url, params: {
+      session: {
+        email: 'Alice@example.com',
+        password: "AlicePassw0rd"
+      }
+    }
+    assert_redirected_to root_url
+    assert_includes flash.keys, 'success'
+  end
+
+  test "should logout if logged in" do
+    # Login Alice
+    post login_url, params: {
+      session: {
+        email: 'Alice@example.com',
+        password: "AlicePassw0rd"
+      }
+    }
+
+    # Logout Alice first time
+    delete logout_url
+    assert_redirected_to root_url
+    assert_includes flash.keys, 'success'
+    follow_redirect!
+
+    # Logout Alice first time
+    delete logout_url
+    assert_redirected_to root_url
+    assert_not_includes flash.keys, 'success'
   end
 end
